096f5e6e47620b90da4bd6598876982d8526e633
Ojas <ojasbendale18200@gmail.com>
2023-12-22 11:51:39 +0530

    setting up the svgs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Sidebar from "./components/Sidebar";
 import "./index.css";
 
+
 function App() {
   return (
     <div className="flex">
---
+++ src/utils/sidebarlinks.js
@@ -0,0 +1,58 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/svg/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/svg/orders.svg",
+    route: "/explore",
+    label: "Orders",
+  },
+  {
+    imgURL: "/svg/products.svg",
+    route: "/all-users",
+    label: "Products",
+  },
+  {
+    imgURL: "/svg/delivery.svg",
+    route: "/saved",
+    label: "Delivery",
+  },
+  {
+    imgURL: "/svg/marketing.svg",
+    route: "/create-post",
+    label: "Marketing",
+  },
+
+  {
+    imgURL: "/svg/analytics.svg",
+    route: "/Analytics",
+    label: "Analytics",
+  },
+  {
+    imgURL: "/svg/payouts.svg",
+    route: "/Payouts",
+    label: "Payouts",
+  },
+  {
+    imgURL: "/svg/discount.svg",
+    route: "/Discounts",
+    label: "Discounts",
+  },
+  {
+    imgURL: "/svg/audiance.svg",
+    route: "/Audiance",
+    label: "Audiance",
+  },
+  {
+    imgURL: "/svg/appearnce.svg",
+    route: "/Appearance",
+    label: "Appearance",
+  },
+  {
+    imgURL: "/svg/plugins.svg",
+    route: "/Plugins",
+    label: "Plugins",
+  },
+];
